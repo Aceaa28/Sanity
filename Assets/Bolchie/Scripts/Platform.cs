a43ecbb64cbdd078a2f97866f02df21c780fd0d8
Aceaa28 <ahernandez3@aoiths.org>
2023-03-14 13:25:25 -0400

    merged from development

--- Assets/Bolchie/Scripts/Platform.cs
+++ Assets/Bolchie/Scripts/Platform.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class Platform : MonoBehaviour
 {
-<<<<<<< HEAD
     float maxY = 0;
     float minY = -9;
     [SerializeField] GameObject platform;
     [SerializeField] GameObject player; 
     Vector2 playerPosition; 
+   [SerializeField] float speed = 1;
 
 
     // Start is called before the first frame update
@@ -19,14 +19,10 @@ public class Platform : MonoBehaviour
         InvokeRepeating("GeneratePlatform", 10.0f, 5.0f);
         
     }
-=======
-   [SerializeField] float speed = 1;
->>>>>>> development-branch
 
     // Update is called once per frame
     void Update()
     {
-<<<<<<< HEAD
         playerPosition = player.transform.position;
       
      
@@ -39,12 +35,10 @@ public class Platform : MonoBehaviour
         Instantiate (platform, new Vector2 (playerPosition.x + 5, Random.Range(minY, maxY)), Quaternion.identity);
         Debug.Log("works");
 
-=======
         transform.position += new Vector3(-speed, 0, 0) * Time.deltaTime;
         if (transform.position.x < -10)
         {
             Destroy(gameObject); 
         }
->>>>>>> development-branch
     }
 }
