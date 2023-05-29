d2503726aceaa05da7c51f65bc956edf3a2507a2
Aceaa28 <ahernandez3@aoiths.org>
2023-05-29 01:03:35 -0400

    enemy animation

---
+++ Assets/Bolchie/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    //define variable and  
+    public float moveRange = 19f;
+    private Vector2 initialPosition;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+       //assign intial position for the enemy
+       initialPosition = transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    // Calculate the movement within the defined range
+    float movement = Mathf.PingPong(Time.time, moveRange) - (moveRange / 2f);
+
+    // Set the new position of the enemy
+    transform.position = initialPosition + new Vector2(movement, 0f);
+    }
+}
