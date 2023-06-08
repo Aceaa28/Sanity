32d177c0c77c756fc726d10200e9ee2b11559465
Samantha1205 <Sandujar@aoiths.org>
2023-06-08 17:30:56 -0400

    holdup

--- Assets/Bolchie/Scripts/DialogueSystem/DialogueHolder.cs
+++ Assets/Bolchie/Scripts/DialogueSystem/DialogueHolder.cs
@@ -6,16 +6,13 @@ namespace DialogueSystem
     public class DialogueHolder : MonoBehaviour
     {
         private IEnumerator dialogueSeq;
-        private IEnumerator dialogueSeq2;
         private bool dialogueFinished;
         public GameObject dialogueTrigger;
 
         private void Awake()
         {
             dialogueSeq = dialogueSequence();
-            dialogueSeq2 = dialogueSequence2();
             StartCoroutine(dialogueSeq);
-            StartCoroutine(dialogueSeq2);
         }
 
         private void Update()
@@ -25,7 +22,6 @@ namespace DialogueSystem
                 Deactivate();
                 gameObject.SetActive(false);
                 StopCoroutine(dialogueSeq);
-                StopCoroutine(dialogueSeq2);
             }
         }
 
@@ -44,29 +40,6 @@ namespace DialogueSystem
                     }
                     yield return new WaitUntil(() => transform.GetChild(i).GetComponent<DialogueLine>().finished);
             }
-        }
-
-        private IEnumerator dialogueSequence2()
-        {
-            if (!dialogueFinished)
-            {
-                for (int i = 0; i < transform.childCount - 1; i++)
-                {
-                    Deactivate();
-                    transform.GetChild(i).gameObject.SetActive(true);
-                    yield return new WaitUntil(() => transform.GetChild(i).GetComponent<DialogueLine>().finished);
-                }
-            }
-            else
-            {
-                int index = transform.childCount - 1;
-                Deactivate();
-                transform.GetChild(index).gameObject.SetActive(true);
-                yield return new WaitUntil(() => transform.GetChild(index).GetComponent<DialogueLine>().finished);
-            }
-
-            dialogueFinished = true;
-            gameObject.SetActive(false);
         }
 
         private void Deactivate()
